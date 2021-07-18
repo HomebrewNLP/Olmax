4706b3857d9cec7f0a4aee101c8b3ca9851baf50
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-07-18 22:59:35 +0200

    feat(model): add basic layers + get_or_create_parameter

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import time
 import typing
 
 import jax
@@ -6,7 +7,7 @@ import jax.lax as lax
 import jax.numpy as jnp
 import jax.random as random
 import numpy as np
-import time
+
 
 class Context:
     def __init__(self):
@@ -14,56 +15,85 @@ class Context:
         self.prng_key = random.PRNGKey(self.seed)
         self.learning_rate = 1e-3
         self.parameters: typing.List[jnp.ndarray] = []
+        self.parameter_dict: typing.Dict[str:jnp.ndarray] = {}
         self.device_steps = 200
         self.steps = 10
+        self.features_per_head = 16
+        self.head_count = 1
+        self.group_linear_factor = 2
+        self.batch_size = 7
+        self.base = self.features_per_head * self.head_count
+        self.out = self.base * self.group_linear_factor
+        self.dtype = jnp.float32
+        self.init_scale = 1.0
 
 
-def orthogonal_init(ctx: Context, shape: typing.List[int], scale=1.0, column_axis=-1, dtype=jnp.float32):
+def orthogonal_init(ctx: Context, shape: typing.List[int], column_axis=-1, ) -> jnp.ndarray:
     n_rows, n_cols = util.prod(shape) // shape[column_axis], shape[column_axis]
     matrix_shape = (n_rows, n_cols) if n_rows > n_cols else (n_cols, n_rows)
-    out, r = jnp.linalg.qr(random.normal(ctx.prng_key, matrix_shape, dtype))
-    out *= lax.broadcast_to_rank(jnp.sign(jnp.diag(r)), rank=out.ndim) * scale
+    out, r = jnp.linalg.qr(random.normal(ctx.prng_key, matrix_shape, ctx.dtype))
+    out *= lax.broadcast_to_rank(jnp.sign(jnp.diag(r)), rank=out.ndim) * ctx.init_scale
     if n_rows < n_cols:
         out = out.T
     return jnp.moveaxis(jnp.reshape(out, tuple(np.delete(shape, column_axis)) + (shape[column_axis],)), -1, column_axis)
 
 
-def compute(params: typing.List[jnp.ndarray]) -> jnp.ndarray:
-    a, b = params
-    c = jnp.einsum("ij,jk->ik", a, b)
-    return c.mean()
+def get_or_create_parameter(ctx: Context, name: str, shape: typing.Optional[typing.List[int]] = None) -> jnp.ndarray:
+    if name not in ctx.parameter_dict:
+        ctx.parameter_dict[name] = orthogonal_init(ctx, shape)
+    return ctx.parameter_dict[name]
 
 
-def update(ctx: Context, grads: typing.List[jnp.ndarray]):
-    return [p - g * ctx.learning_rate for p, g in zip(ctx.parameters, grads)]
+def linear(inp: jnp.ndarray, params: Context, name: str) -> jnp.ndarray:
+    shape = [params.out, params.base]
+    if inp.shape[-1] == params.base:
+        shape = shape[::-1]
+    spec = ''.join(chr(ord('a') + i) for i in range(inp.ndim))
+    return jnp.einsum(f'{spec},{spec[-1]}z->{spec[:-1]}z', inp, get_or_create_parameter(params, name, shape))
 
 
-def train_step(ctx: Context):
+def input_embedding(params: Context, name: str) -> jnp.ndarray:
+    return get_or_create_parameter(params, name, [params.batch_size, params.base])
+
+
+def relu(inp: jnp.ndarray) -> jnp.ndarray:
+    return jnp.maximum(inp, 0)
+
+
+def compute(params: typing.Dict[str, jnp.ndarray]) -> jnp.ndarray:
+    ctx = Context()
+    ctx.parameter_dict = params
+    return linear(relu(linear(input_embedding(ctx, "embd"), ctx, "lin0")), ctx, "lin1").mean()
+
+
+def update(ctx: Context, grads: typing.List[jnp.ndarray]) -> typing.Dict[str, jnp.ndarray]:
+    return {k: p - g * ctx.learning_rate for (k, p), g in zip(ctx.parameter_dict.items(), grads.values())}
+
+
+def train_step(ctx: Context) -> typing.Tuple[jnp.ndarray, typing.Dict[str, jnp.ndarray]]:
     grad_fn = jax.value_and_grad(compute, 0)
-    loss, grads = grad_fn(ctx.parameters)
+    loss, grads = grad_fn(ctx.parameter_dict)
     return loss, update(ctx, grads)
 
 
 @jax.jit
-def step(parameters: typing.List[jnp.ndarray]) -> typing.Tuple[jnp.ndarray, typing.List[jnp.ndarray]]:
+def step(parameter_dict: typing.Dict[str,jnp.ndarray]) -> typing.Tuple[jnp.ndarray, typing.Dict[str, jnp.ndarray]]:
     ctx = Context()
-    ctx.parameters = parameters
+    ctx.parameter_dict = parameter_dict
     loss = jnp.zeros([])
     for _ in range(ctx.device_steps):
-        out, ctx.parameters = train_step(ctx)
+        out, ctx.parameter_dict = train_step(ctx)
         loss += out
-    return loss / ctx.device_steps, ctx.parameters
+    return loss / ctx.device_steps, ctx.parameter_dict
 
 
 def main():
     ctx = Context()
-    a = orthogonal_init(ctx, [3, 5])
-    b = orthogonal_init(ctx, [5, 2])
-
-    parameters = [a, b]
+    compute(ctx.parameter_dict)
+    parameter_dict = ctx.parameter_dict
     for _ in range(ctx.steps):
         start_time = time.time()
-        loss, parameters = step(parameters)
+        loss, parameter_dict = step(parameter_dict)
         print(loss, time.time() - start_time)
 
 
