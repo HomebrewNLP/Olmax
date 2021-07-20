19d6c9690a5c4c8967d4135366d7da8a61c49d01
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-07-20 12:03:49 +0200

    fix(model): sum output in model

--- model.py
+++ model.py
@@ -133,7 +133,7 @@ def cross_entropy_loss(ctx: Context, src: jnp.ndarray, tgt: jnp.ndarray) -> jnp.
     max_src = lax.stop_gradient(src).max(-1, keepdims=True)
     log_z = jnp.log(jnp.exp(src - max_src).sum(-1, keepdims=True)) + max_src
     loss = jnp.einsum(f"{spec},{spec}->", src + log_z, one_hot(tgt, ctx.data.vocab_size))
-    return (jnp.square(log_z) * ctx.z_loss - loss) / tgt.size
+    return (jnp.square(log_z).sum() * ctx.z_loss - loss) / tgt.size
 
 
 def compute_ctx(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
