fix(model): use mean in norm normalization

model.py
@@ -289,7 +289,7 @@ def instance_norm(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
     ctx = ctx.add_to_prefix("instance_norm")
     shape = ["one"] * (inp.ndim - 2) + [ctx.dims.heads, get_feature_dim(ctx, inp)]
     inp = inp - shard(inp.mean(-1, keepdims=True), None)
-    scale = lax.rsqrt(ctx.model.norm_eps + shard(jnp.square(inp).sum(-1, keepdims=True), None))
+    scale = lax.rsqrt(ctx.model.norm_eps + shard(jnp.square(inp).mean(-1, keepdims=True), None))
     scale = scale * get_param(ctx, "scale", shape, ctx.model.initializer.norm_std, 1)
     return scale * inp + get_param(ctx, "shift", shape, ctx.model.initializer.norm_std)
 