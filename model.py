fix(model/attention): sum over correct dims in base_grad calculation

model.py
@@ -360,9 +360,9 @@ def attention_op(src: jnp.ndarray, base_param: jnp.ndarray, key_param: jnp.ndarr
             q_p_grad = dot_general(base, qry_grad, batch_seq, batch_dims + (head_dim,), tuple(), tuple())
             v_p_grad = dot_general(base, val_grad, batch_seq, batch_dims + (feature_dim,), tuple(), tuple())
 
-            base_grad = dot_general(key_grad, k_p, (feature_dim,), (1,), tuple(), tuple())
-            base_grad += dot_general(val_grad, v_p, (feature_dim,), (1,), tuple(), tuple())
-            base_grad += dot_general(qry_grad, q_p, (head_dim,), (0,), tuple(), tuple())
+            base_grad = dot_general(key_grad, k_p, (sequence_dim, feature_dim), (1, 2), tuple(), tuple())
+            base_grad += dot_general(val_grad, q_p, (sequence_dim, feature_dim), (1, 2), tuple(), tuple())
+            base_grad += dot_general(qry_grad, v_p, (sequence_dim, head_dim), (1, 2), tuple(), tuple())
 
             base_grad = jnp.where(base > 0, base_grad, jnp.zeros_like(base_grad))
             base_grad = dot_general(base_grad, b_p, (feature_dim,), (feature_dim,), tuple(), tuple())