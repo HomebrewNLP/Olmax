6edf2fa4bf0b3f106db2e05ee8365030400cdf05
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-07-24 22:14:26 +0200

    fix(model/attention): swap b_p_grad dot_product inputs

--- model.py
+++ model.py
@@ -367,11 +367,10 @@ def attention_op(src: jnp.ndarray, base_param: jnp.ndarray, key_param: jnp.ndarr
             base_grad += dot_general(val_grad, v_p, (sequence_dim, head_dim), (1, 2), tuple(), tuple())
 
             base_grad = jnp.where(inp >= 0, base_grad, jnp.zeros_like(base_grad))
-            base_grad = dot_general(base_grad, b_p, (head_dim,), (2,), tuple(), tuple())
-
+            inp_grad = dot_general(base_grad, b_p, (head_dim,), (2,), tuple(), tuple())
             b_p_grad = dot_general(inp, base_grad, batch_seq, batch_seq, tuple(), tuple())
 
-            return base_grad, b_p_grad, k_p_grad, q_p_grad, v_p_grad
+            return inp_grad, b_p_grad, k_p_grad, q_p_grad, v_p_grad
 
         return out, grad_fn
 
