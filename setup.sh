1c509fea1f4100d8a5a5f51495c6d876fbb9c9d0
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-07-25 14:51:13 +0200

    fix(model): encode input-instance-norm

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip==21.1.3  # Fix as soon as pip is stable. Right now, 21.2 is broken
 python3 -m pip install --upgrade tensorflow==2.4.2 jsonpickle
 python3 -m pip install --upgrade "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
