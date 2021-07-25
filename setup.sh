8fab084999a10b68cfc71f5e812408223734fc16
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-07-25 02:26:29 +0200

    feat(setup): add working tensorboard

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade tensorflow==2.4.2 jsonpickle
 python3 -m pip install --upgrade "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile
+python3 -m pip install tensorboard==2.4.0
+python3 -m pip install tensorboard-plugin-profile
