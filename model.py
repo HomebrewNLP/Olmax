fix(model/group_ff): use correct dot products in gradient (+ avoid transpose!)

model.py
@@ -189,6 +189,7 @@ def group_feed_forward(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
         return inp
 
     transpose = tuple(range(1, ndim - 1)) + (0, ndim - 1)
+    batch_seq = tuple(range(ndim - 2))
     batch_seq_1 = tuple(range(1, ndim - 1))
 
     @jax.custom_gradient
@@ -198,13 +199,11 @@ def group_feed_forward(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
         out = shard(out.transpose(transpose))
 
         def _grad_fn(dy: jnp.ndarray) -> typing.Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-            dy_t = dy.transpose(transpose)
             r_mid = relu(mid)
-            o_w_grad = dot_general(r_mid, dy_t, batch_seq_1, batch_seq_1, (0,), (0,))
-            d_mid = dot_general(dy_t, out_weight, (dy_t.ndim - 1,), (2,), (0,), (0,))
+            o_w_grad = dot_general(r_mid, dy, batch_seq_1, batch_seq, (0,), (2,))
+            d_mid = dot_general(dy, out_weight, (ndim - 1,), (2,), (ndim - 2,), (0,))
             d_mid = d_mid * jnp.greater(mid, 0).astype(ctx.model.dtype)
-            d_mid = d_mid.transpose(transpose)
-            i_w_grad = dot_general(d_mid, src, batch_seq_1, batch_seq_1, (0,), (0,))
+            i_w_grad = dot_general(src, d_mid, batch_seq, batch_seq_1, (ndim - 2,), (0,))
             d_src = dot_general(d_mid, i_w, (d_mid.ndim - 1,), (2,), (0,), (0,))
             d_src = d_src.transpose(transpose)
             return d_src, i_w_grad, o_w_grad