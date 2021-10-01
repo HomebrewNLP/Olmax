aa8895e353bbda547b5de59a5cb5d92e927ca9d6
JackMcCoy <fdrdavegill@gmail.com>
2021-10-01 18:07:19 -0400

    reformat loss

--- src/utils/wandb.py
+++ src/utils/wandb.py
@@ -10,7 +10,7 @@ class WandbLog:
         self.run = run
 
     def __call__(self, wctx, current_lr):
-        curr_loss = wctx.current_loss.astype(float) / wctx.ctx.wandb.log_frequency
+        curr_loss = wctx.loss / wctx.ctx.wandb.log_frequency
         step = self.idx * wctx.ctx.wandb.log_frequency * wctx.ctx.training.device_steps
         self.idx += 1
         self.mean_loss = ((wctx.loss / step) * self.prev + curr_loss * self.idx) / (self.prev + self.idx)  # LWMA
@@ -19,10 +19,10 @@ class WandbLog:
         rate = step / (time.time() - self.start_time)
         tokens_per_day = step * 3600 * 24 * rate * wctx.ctx.dims.sizes.batch * wctx.ctx.dims.sizes.sequence
 
-        self.run.log({"Loss/Current": curr_loss,
-                   "Loss/Mean": self.mean_loss,
+        self.run.log({"Loss/Current": curr_loss.astype(float),
+                   "Loss/Mean": self.mean_loss.astype(float),
                    "Speed/Batches per Second": rate,
                    "Speed/Tokens per Day": tokens_per_day,
-                   "Optimizer/Learning Rate": current_lr,
+                   "Optimizer/Learning Rate": current_lr.astype(float),
                    "Optimizer/Beta1": wctx.ctx.model.momentumnet_beta},
                   step=step)
