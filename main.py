6ac7ca27e3e8b5364c8c6dfa2afd9cf13e392e03
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-02-13 23:27:07 +0100

    fix(main): only annotate existing variances

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def main():
                                 for name, dims in ctx.parameter_dims.items()},
                  'data': None,
                  'current_step': None, 'loss': None, 'top_loss': None,
-                 'parameter_variance': {name: None for name, dims in ctx.parameter_dims.items()}
+                 'parameter_variance': {name: None for name, dims in ctx.parameter_variance.items()}
                  }
     step = train_loop(wctx, timeit(f"PMapping across {ParallelAxes.model}", jax.pmap, jitless_step, ParallelAxes.model,
                                    in_axes=(partition,), out_axes=partition))
