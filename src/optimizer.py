fix(optimizer): flip sign in weight decay

src/optimizer.py
@@ -68,5 +68,5 @@ def update(ctx: Context, grads: typing.Dict[str, jnp.ndarray], current_step: jnp
         updated_weight = adam(inner_ctx, param_name, grad, current_step)
         parameter_lr = lr * ctx.parameter_variance.get(param_name, 1)
         updated_weight *= parameter_lr
-        updated_weight += (1 - ctx.optimizer.weight_decay * parameter_lr) * ctx.parameters[param_name]
+        updated_weight += (1 + ctx.optimizer.weight_decay * parameter_lr) * ctx.parameters[param_name]
         ctx.parameters[param_name] = updated_weight