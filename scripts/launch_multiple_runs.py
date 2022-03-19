5457099e1da29ac032d4924abd614beb6e592d69
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-03-19 19:40:04 +0100

    fix(launch_multiple): use nohup

--- scripts/launch_multiple_runs.py
+++ scripts/launch_multiple_runs.py
@@ -9,38 +9,41 @@ from tpunicorn.tpu import list_tpus
 
 
 def exec_tpu(host: str, command: str):
-    os.system(f"gcloud alpha compute tpus tpu-vm ssh ubuntu@{host} --command {command}")
+    print(f"running {command}..", end='')
+    start_time = time.time()
+    os.system(f"gcloud alpha compute tpus tpu-vm ssh ubuntu@{host} --command '{command}'")
+    print(f"done after {time.time() - start_time:.1f}s")
 
 
 def exec_code(host: str, wandb_key: str, sweep_id: str):  # https://wandb.ai/authorize
     exec_tpu(host, f"sudo apt --fix-missing --fix-broken install -y git python3 python3-pip")
     exec_tpu(host, "git clone --depth 1 https://github.com/HomebrewNLP/HomebrewNLP-Jax/")
-    exec_tpu(host, "cd HomebrewNLP-Jax && sudo bash setup.sh")
+    exec_tpu(host, "cd HomebrewNLP-Jax && bash setup.sh")
     exec_tpu(host, f"wandb login {wandb_key}")
-    exec_tpu(host, f"screen -S model bash -c 'cd HomebrewNLP-Jax && wandb agent --count 1 {sweep_id} ; "
-                   f"python3 -c 'from google.cloud import tpu_v2alpha1; "
-                   f"tpu_v2alpha1.TpuClient().delete_node(request=tpu_v2alpha1.DeleteNodeRequest(name={host}))"
-                   f".result()'")  # Delete via python client as pu doesn't work and gcloud needs manual confirmation
+    exec_tpu(host, f'nohup bash -c "cd HomebrewNLP-Jax && wandb agent --count 1 {sweep_id} ; '
+                   f'python3 -c \\"from google.cloud import tpu_v2alpha1; '
+                   f'tpu_v2alpha1.TpuClient().delete_node(request=tpu_v2alpha1.DeleteNodeRequest(name={host}))'
+                   f'.result()\\"" &> log.txt 2> error.txt &')
 
 
-def tpu_names(zone: str, project: str):
-    return [t['name'].split('/')[-1] for t in list_tpus(zone, project)]
+def tpu_names(zone: str):
+    return [t['name'].split('/')[-1] for t in list_tpus(zone)]
 
 
-def start_single(prefix: str, tpu_id: int, sweep_id: str, wandb_key: str, tpu_version: int, zone: str, project: str):
+def start_single(prefix: str, tpu_id: int, sweep_id: str, wandb_key: str, tpu_version: int, zone: str):
     tpu_name = f"{prefix}-{tpu_id}"
     while True:
-        os.system(f'while ! gcloud alpha compute tpus tpu-vm create {tpu_name} '
-                  f'--zone {zone} --accelerator-type v{tpu_version}-8 --version v2-alpha --preemptible; '
-                  f'do echo "Trying again.."; done')
+        # os.system(f'while ! gcloud alpha compute tpus tpu-vm create {tpu_name} '
+        #           f'--zone {zone} --accelerator-type v{tpu_version}-8 --version v2-alpha --preemptible; '
+        #           f'do echo "Trying again.."; done')
         exec_code(tpu_name, wandb_key, sweep_id)
-        while tpu_name in tpu_names(zone, project):
+        while tpu_name in tpu_names(zone):
             time.sleep(60)
 
 
 def start_multiple(prefix: str, tpus: int, sweep_id: str, tpu_version: int = 3, zone: str = "europe-west4-a"):
     _, _, wandb_key = netrc.netrc().authenticators("api.wandb.ai")
-    threads = [threading.Thread(target=start_single, args=(prefix, tpu_id, sweep_id, wandb_key, tpu_version, zone))
+    threads = [threading.Thread(target=start_single, args=(prefix, tpu_id + 1, sweep_id, wandb_key, tpu_version, zone))
                for tpu_id in range(tpus)]
     for t in threads:
         t.start()
