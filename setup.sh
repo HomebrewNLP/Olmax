7f07de0e6bd428dacaca296727078bcdf0b52fce
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-03-19 19:39:51 +0100

    fix(setup.sh) add sudo to uninstall

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade "jax[tpu]>=0.3.0" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
+sudo python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
 python3 -m pip install wandb smart-open jsonpickle tpunicorn google-api-python-clientp google-cloud-tpu
 python3 -m pip install --upgrade --force-reinstall tensorflow==2.8.0
