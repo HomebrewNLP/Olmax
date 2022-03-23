c7c30a3c56b56e5ff1bdf419b7e00c9317108694
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-03-23 17:54:23 +0100

    fix(launch_multiple): many things
    * transfer file instead of many executions (otherwise it'd keep starting things with dead tpus)
    * use multiprocessing instead of multithreading
    * check if tpus are relevant before launching threads in delete_all
    * make sure not to create tpu when still deleting
    * add some quality-of-life prints

--- scripts/launch_multiple_runs.py
+++ scripts/launch_multiple_runs.py
@@ -1,7 +1,8 @@
 import argparse
+import datetime
+import multiprocessing
 import netrc
 import os
-import sys
 import threading
 import time
 import typing
@@ -11,6 +12,26 @@ from tpunicorn.tpu import list_tpus
 TIMEOUT_MULTIPLIER = 10
 
 
+def exec_command(wandb_key: str, sweep_id: str, host: str, zone: str):
+    # Bottom one doesn't use , on purpose
+    return ' && '.join((f"sudo apt --fix-missing --fix-broken install -y git python3 python3-pip",
+                        f"(rm -rf HomebrewNLP-Jax ; pkill -f python3 ; exit 0)",
+                        f"git clone --depth 1 https://github.com/HomebrewNLP/HomebrewNLP-Jax/",
+                        f"cd HomebrewNLP-Jax",
+                        f"(bash setup.sh ; exit 0)",
+                        f"/home/ubuntu/.local/bin/wandb login {wandb_key}",
+                        f'screen -dmS model bash -c "cd HomebrewNLP-Jax ; /home/ubuntu/.local/bin/wandb agent --count 1'
+                        f' {sweep_id} ; echo y | gcloud alpha compute tpus tpu-vm delete {host} --zone {zone}" || '
+                        f'echo y | gcloud alpha compute tpus tpu-vm delete {host} --zone {zone}'))
+
+
+def send_commands_to_tpu(wandb_key: str, sweep_id: str, host: str, zone: str):
+    command = exec_command(wandb_key, sweep_id, host, zone)
+    with open(f'.{host}.sh', 'w') as f:
+        f.write(command)
+    os.system(f"gcloud alpha compute tpus tpu-vm scp .{host}.sh ubuntu@{host}:~/setup.sh --zone {zone}")
+
+
 def exec_tpu(host: str, zone: str, command: str):
     print(f"running '{command}' ...", end='')
     start_time = time.time()
@@ -18,23 +39,31 @@ def exec_tpu(host: str, zone: str, command: str):
     print(f"done after {time.time() - start_time:.1f}s")
 
 
-def tpu_names(zone: str, preempted: bool = True):
-    return [t['name'].split('/')[-1] for t in list_tpus(zone) if preempted or t['state'] != "PREEMPTED"]
+def tpu_names(zone: str, preempted: bool = True, deleting: bool = False, prefix: str = ''):
+    while True:
+        try:
+            tpus = [t['name'].split('/')[-1] for t in list_tpus(zone) if
+                    (deleting or t['state'] != "DELETING") and (preempted or t['state'] != "PREEMPTED")]
+            return [t for t in tpus if t.startswith(prefix)]
+        except:
+            pass
 
 
 def delete_one_tpu(prefix: str, host: str, zone: str):
     if prefix not in host:
         return
+    print(f"\x1b[32;1m  DELETING {host}\x1b[0m")
     os.system(f"echo y | gcloud alpha compute tpus tpu-vm delete {host} --zone {zone} --async")
 
 
 def delete_all(prefix: str, zone: str):
-    threads = [threading.Thread(target=delete_one_tpu, args=(prefix, host, zone), daemon=True) for host in
-               tpu_names(zone)]
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
+    while tpu_names(zone, prefix=prefix):
+        threads = [threading.Thread(target=delete_one_tpu, args=(prefix, host, zone), daemon=True) for host in
+                   tpu_names(zone)]
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
 
 
 def start_single(prefix: str, tpu_id: int, tpus: int, sweep_id: str, wandb_key: str, tpu_version: int, zone: str):
@@ -46,34 +75,37 @@ def start_single(prefix: str, tpu_id: int, tpus: int, sweep_id: str, wandb_key: 
                       f'--zone {zone} --accelerator-type v{tpu_version}-8 --version v2-alpha --preemptible; '
                       f'do sleep {tpus * TIMEOUT_MULTIPLIER}; done')
 
-            exec_tpu(host, zone, f"sudo apt --fix-missing --fix-broken install -y git python3 python3-pip")
-            exec_tpu(host, zone, "rm -rf HomebrewNLP-Jax ; !pkill -f python3")
-            exec_tpu(host, zone, "git clone --depth 1 https://github.com/HomebrewNLP/HomebrewNLP-Jax/")
-            exec_tpu(host, zone, "cd HomebrewNLP-Jax && bash setup.sh")
-            exec_tpu(host, zone, f"/home/ubuntu/.local/bin/wandb login {wandb_key}")
-            exec_tpu(host, zone, f'screen -dmS model bash -c "cd HomebrewNLP-Jax && '
-                                 f'/home/ubuntu/.local/bin/wandb agent --count 1 {sweep_id} ; '
-                                 f'echo y | gcloud alpha compute tpus tpu-vm delete {host} --zone {zone}"')
+            send_commands_to_tpu(wandb_key, sweep_id, host, zone)
+            exec_tpu(host, zone, "bash setup.sh")
 
             while host in tpu_names(zone, False):
-                time.sleep(60)
+                print(f"{host} - {datetime.datetime.now()}: SLEEPING")
+                time.sleep(5)
+
             delete_one_tpu(prefix, host, zone)
+            while host in tpu_names(zone, deleting=True):
+                time.sleep(2)
         except KeyboardInterrupt:
-            sys.exit()
+            print(f"{host} - {datetime.datetime.now()}: KeyboardInterrupt received. Killing TPU, then self.")
+            delete_one_tpu(prefix, host, zone)
+            return
 
 
 def start_multiple(prefix: str, tpus: int, sweep_id: str, tpu_version: int = 3, zone: str = "europe-west4-a"):
     _, _, wandb_key = netrc.netrc().authenticators("api.wandb.ai")
-    threads = [
-        threading.Thread(target=start_single, args=(prefix, tpu_id + 1, tpus, sweep_id, wandb_key, tpu_version, zone),
-                         daemon=True) for tpu_id in range(tpus)]
-    for t in threads:
-        t.start()
-    while all(t.is_alive() for t in threads):
+    procs = []
+    for tpu_id in range(tpus):
+        proc = multiprocessing.Process(target=start_single, daemon=True,
+                                       args=(prefix, tpu_id + 1, tpus, sweep_id, wandb_key, tpu_version, zone))
+        proc.start()
+        procs.append(proc)
+    while all(t.is_alive() for t in procs):
         try:
             time.sleep(10)
         except KeyboardInterrupt:
-            sys.exit()
+            print(f"MAIN - {datetime.datetime.now()}: KeyboardInterrupt received. Killing All TPUs, then self.")
+            delete_all(prefix, zone)
+            return
 
 
 def parse_args() -> typing.Tuple[int, int, str, str, str, bool]:
