33b0a10c32c97f0d208cf436f7872b1f2e755063
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-04-06 06:47:53 +0200

    fix(wandb): multiply median size by device steps

--- src/utils/wandb.py
+++ src/utils/wandb.py
@@ -9,27 +9,28 @@ class WandbLog:
     def __init__(self, run):
         self.start_time = time.time()
         self.run = run
-        self.last_losses = []
-        self.last_accuracy = []
+        self.losses = []
+        self.accuracies = []
         self.idx = 0
 
     def __call__(self, wctx: WhileContext, current_lr):
         self.idx += 1
         ctx = wctx.ctx
-        curr_loss = wctx.loss / ctx.training.device_steps
-        step = self.idx * ctx.wandb.log_frequency * ctx.training.device_steps
+        device_steps = ctx.training.device_steps
+        curr_loss = wctx.loss / device_steps
+        step = self.idx * ctx.wandb.log_frequency * device_steps
         sizes = [s // ctx.training.device_steps for s in ctx.wandb.median_sizes]
-        self.last_losses.append(curr_loss.astype(float))
-        self.last_accuracy.append((wctx.top_loss / ctx.training.device_steps).astype(float))
-        self.last_losses = self.last_losses[-max(sizes):]
-        self.last_accuracy = self.last_accuracy[-max(sizes):]
-        self.run.log({f"Loss/Median{s}": np.median(self.last_losses[-s:]) for s in sizes}, step=step)
-        self.run.log({f"Accuracy/Median{s}": np.median(self.last_accuracy[-s:]) for s in sizes}, step=step)
+        self.losses.append(curr_loss.astype(float))
+        self.accuracies.append((wctx.top_loss / device_steps).astype(float))
+        self.losses = self.losses[-max(sizes):]
+        self.accuracies = self.accuracies[-max(sizes):]
+        self.run.log({f"Loss/Median{s * device_steps}": np.median(self.losses[-s:]) for s in sizes}, step=step)
+        self.run.log({f"Accuracy/Median{s * device_steps}": np.median(self.accuracies[-s:]) for s in sizes}, step=step)
 
         rate = step / (time.time() - self.start_time)
         tokens_per_day = 3600 * 24 * rate * ctx.dims.sizes.batch * ctx.dims.sizes.sequence
 
-        self.run.log({"Loss/Current": self.last_losses[-1], "Accuracy/Current": self.last_accuracy[-1],
+        self.run.log({"Loss/Current": self.losses[-1], "Accuracy/Current": self.accuracies[-1],
                       "Speed/Batches per Second": rate, "Speed/Tokens per Day": tokens_per_day,
                       "Optimizer/Learning Rate": current_lr.astype(float), "Optimizer/Beta1": ctx.optimizer.adam_beta1,
                       "Optimizer/Beta2": ctx.optimizer.adam_beta2}, step=step)
