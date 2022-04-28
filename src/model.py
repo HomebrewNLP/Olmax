1f6e4d9ce1e2d41a475c4f102de72ec8c8ee8721
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-04-28 13:54:54 +0200

    fix(model): don't init to 0

--- src/model.py
+++ src/model.py
@@ -73,7 +73,8 @@ def rezero(ctx: Context, inp: jnp.ndarray, scale: float = 1) -> jnp.ndarray:
 def conv(ctx: Context, inp: jnp.ndarray, conv_kernel: str, scale: float, in_features: str, out_features: str):
     ctx = ctx.add_to_prefix("conv")
     fan_in = ctx.dims.sizes[in_features] * ctx.dims.sizes[conv_kernel]
-    weight = get_param(ctx, "weight", [out_features, in_features, conv_kernel], column_axes=2, scale=scale / fan_in)
+    weight = get_param(ctx, "weight", [out_features, in_features, conv_kernel], column_axes=2, scale=scale,
+                       lr_scale=1 / fan_in)
 
     if ctx.is_initializing:
         return jnp.zeros(inp.shape[:-1] + (ctx.dims.sizes[out_features],))
