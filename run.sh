874454f3a215f60afc1eb50880778172cb11788f
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-05-12 01:08:33 +0200

    fix(run): allow fp64 for shampoo

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ export TF_CPP_MIN_LOG_LEVEL=4
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 export XLA_FLAGS="--xla_force_host_platform_device_count=48"
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=60000000000
+export JAX_ENABLE_X64=1
 /usr/bin/env python3 main.py "$@"
