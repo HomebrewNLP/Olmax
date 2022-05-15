567d79f0e7faabbcbe309a59e2e1c6a541f62b02
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-05-15 15:40:28 +0200

    feat(start_multihost): allow branch switching

--- script/start_on_multi_host_tpu.py
+++ script/start_on_multi_host_tpu.py
@@ -7,12 +7,13 @@ import typing
 from launch_multiple_runs import all_tpus
 
 
-def parse_args() -> typing.Tuple[str, str]:
+def parse_args() -> typing.Tuple[str, str, str]:
     parser = argparse.ArgumentParser()
     parser.add_argument("--tpu", type=str, help="Name of the TPU to use")
     parser.add_argument("--zone", type=str, help="Where the TPU is")
+    parser.add_argument("--branch", type=str, help="Git branch to use")
     args = parser.parse_args()
-    return args.tpu, args.zone
+    return args.tpu, args.zone, args.branch
 
 
 def install(zone: str, name: str, worker: int):
@@ -26,13 +27,16 @@ def install(zone: str, name: str, worker: int):
 
 
 def main():
-    name, zone = parse_args()
+    name, zone, branch = parse_args()
     _, _, wandb_key = netrc.netrc().authenticators("api.wandb.ai")
     tpu = [tpu for tpu in all_tpus(zone) if tpu['name'].split('/')[-1] == name][0]
     hosts = len(tpu['networkEndpoints'])
     with open("exec.sh", "w") as f:
         f.write("git clone https://github.com/HomebrewNLP/HomebrewNLP-Jax/ ; "
                 "cd HomebrewNLP-Jax ; "
+                f"git fetch ; "
+                f"git checkout {branch} ; "
+                f"git pull ; "
                 "bash setup.sh ; "
                 f"/home/ubuntu/.local/bin/wandb login {wandb_key} ; "
                 "screen -dmS model bash run.sh")
