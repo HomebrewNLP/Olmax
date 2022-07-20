c03d9ca60899f40058b1d4c09b3df018e690ef63
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-07-20 17:09:37 +0200

    fix(wandb): log first

--- src/utils/wandblog.py
+++ src/utils/wandblog.py
@@ -28,6 +28,19 @@ class WandbLog:
         self.accuracies = self.accuracies[-max(sizes):]
         self.loss_medians = self.loss_medians[-max(sizes):]
 
+        self.run.log({f"Loss/Median{s * self.device_steps}": np.median(self.losses[-s:]) for s in sizes}, step=step)
+        self.run.log({f"Accuracy/Median{s * self.device_steps}": np.median(self.accuracies[-s:]) for s in sizes},
+                     step=step)
+
+        rate = step / (time.time() - self.start_time)
+        tokens_per_day = 3600 * 24 * rate * ctx.dims.batch * ctx.dims.sequence
+
+        self.run.log({"Loss/Current": self.losses[-1], "Accuracy/Current": self.accuracies[-1],
+                      "Speed/Batches per Second": rate, "Speed/Tokens per Day": tokens_per_day,
+                      "Optimizer/Learning Rate": current_lr.astype(float), "Optimizer/Beta1": ctx.optimizer.adam_beta1,
+                      "Optimizer/Beta2": ctx.optimizer.adam_beta2
+                      }, step=step)
+
         es = ctx.training.early_stopping
         if self.loss_medians[0] < (self.loss_medians[-1] * (1 - es.minimum_relative_loss_change)):
             print(f"Not Improving | Oldest Loss Median: {self.loss_medians[0]:9.6f} - "
@@ -39,18 +52,4 @@ class WandbLog:
                   f"Last Losses: {self.losses[-es.maximum_spike_duration // self.device_steps:]}")
             return True
 
-        if self.run is None:
-            return False
-
-        self.run.log({f"Loss/Median{s * self.device_steps}": np.median(self.losses[-s:]) for s in sizes}, step=step)
-        self.run.log({f"Accuracy/Median{s * self.device_steps}": np.median(self.accuracies[-s:]) for s in sizes}, step=step)
-
-        rate = step / (time.time() - self.start_time)
-        tokens_per_day = 3600 * 24 * rate * ctx.dims.batch * ctx.dims.sequence
-
-        self.run.log({"Loss/Current": self.losses[-1], "Accuracy/Current": self.accuracies[-1],
-                      "Speed/Batches per Second": rate, "Speed/Tokens per Day": tokens_per_day,
-                      "Optimizer/Learning Rate": current_lr.astype(float), "Optimizer/Beta1": ctx.optimizer.adam_beta1,
-                      "Optimizer/Beta2": ctx.optimizer.adam_beta2
-                      }, step=step)
         return False
