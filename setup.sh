e943deabd637e4427dae2d0e8b43b57096883c2e
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-07-21 07:50:12 +0200

    fix(setup): TPUs already have git but become unstable when installing it

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade "jax[tpu]>=0.3.10" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
-sudo apt-get -o DPkg::Lock::Timeout=-1 update
-sudo apt-get -o DPkg::Lock::Timeout=-1 install -y libpq-dev python-dev python3-dev gcc
 python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils
 python3 -m pip install --upgrade --force-reinstall tensorflow==2.8.0 protobuf==3.20.1
