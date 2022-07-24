5e7e1c5df3af4ea7d1729eed973a8fd9b1274960
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-07-24 10:45:13 +0200

    Revert "Revert "feat(optimizer): psum over clipping""
    This reverts commit 71813adb3d5bfb33da1e38ea13eeb37cf2b06991.

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,9 +1,9 @@
 import typing
 
 import jax
-from jax import numpy as jnp
+from jax import lax, numpy as jnp
 
-from .backend import assign, get_param, maybe_fn, prefixed_name, stable_rsqrt, zero_param
+from .backend import assign, get_param, prefixed_name, stable_rsqrt, zero_param
 from .constants import ParallelAxes
 from .context import Context
 from .shampoo import Preconditioner, fallback_pth_root
@@ -90,7 +90,7 @@ def shampoo(ctx: Context, grad: jnp.ndarray, step: jnp.ndarray) -> jnp.ndarray:
 
 
 def clip_norm(val: jnp.ndarray, min_norm: float) -> jnp.ndarray:
-    return jnp.maximum(jnp.sqrt(jnp.square(val).sum()), min_norm)
+    return jnp.maximum(lax.psum(jnp.sqrt(jnp.square(val).sum()), ParallelAxes.model), min_norm)
 
 
 def adaptive_gradient_clipping(ctx: Context, param_name: str, grad: jnp.ndarray) -> jnp.ndarray:
