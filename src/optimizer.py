21eef7fa6d3966c569561a2a9ca85e2d0fb44620
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2022-08-01 13:39:53 +0200

    test(optimizer): disable heavyball

--- src/optimizer.py
+++ src/optimizer.py
@@ -127,8 +127,7 @@ def update(ctx: Context, grads: typing.Dict[str, jnp.ndarray], step: jnp.ndarray
         update = adam(inner_ctx, grad, step)
         if not small_parameter(param_name, grad):  # Do adam update for small parameters
             shampoo_update = shampoo(inner_ctx, grad, step)
-            shampoo_update = ema(inner_ctx, shampoo_update, step, 1 - ctx.optimizer.momentum_beta, "momentum",
-                                 heavyball=True)
+            shampoo_update = ema(inner_ctx, shampoo_update, step, 1 - ctx.optimizer.momentum_beta, "momentum")
             update = graft(update, shampoo_update)
             ctx.parameters[param_name] = (1 + ctx.optimizer.weight_decay * parameter_lr) * ctx.parameters[param_name]
         update = update.astype(ctx.parameters[param_name].dtype)
