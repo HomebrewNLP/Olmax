7951cca6ddbd67ea3b766ddaa5268ee042442389
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-08-15 09:21:44 +0200

    Revert "test(optimizer): use adam again"
    This reverts commit 9c6979576c2ac5fa2cfc686741b81f426ba0b9c0.

--- src/optimizer.py
+++ src/optimizer.py
@@ -127,13 +127,14 @@ def update(ctx: Context, grads: typing.Dict[str, jnp.ndarray], step: jnp.ndarray
 
         grad = adaptive_gradient_clipping(ctx, param_name, grad)
         grad = graft(grad, jnp.sign(grad) * grad ** 2)
-        update = adam(inner_ctx, grad, step)
-        if not small_parameter(param_name, grad):  # Do adam update for small parameters
+        if small_parameter(param_name, grad):  # Do adam update for small parameters
+            update = adam(inner_ctx, grad, step)
+        else:
+            update = sm3(inner_ctx, param_name, grad)
             if ctx.optimizer.use_shampoo:
                 shampoo_update = shampoo(inner_ctx, grad, step)
-                shampoo_update = ema(inner_ctx, shampoo_update, step, 1 - ctx.optimizer.momentum_beta, "momentum",
-                                     heavyball=True)
                 update = graft(update, shampoo_update)
+            update = ema(inner_ctx, update, step, 1 - ctx.optimizer.momentum_beta, "momentum", heavyball=True)
             ctx.parameters[param_name] = (1 + ctx.optimizer.weight_decay * parameter_lr) * ctx.parameters[param_name]
         update = update.astype(ctx.parameters[param_name].dtype)
         ctx.parameters[param_name] = update * parameter_lr + ctx.parameters[param_name]
