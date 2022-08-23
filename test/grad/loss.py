37dd496b9599681195bf6a643fc6a851d9e8b757
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-08-23 22:29:01 +0200

    feat(test/loss): add check for value

--- test/grad/loss.py
+++ test/grad/loss.py
@@ -1,6 +1,7 @@
 import random
 
 import jax
+import numpy as np
 import pytest
 from jax import lax, numpy as jnp
 
@@ -10,11 +11,18 @@ from src.context import Context
 from src.model.loss import cross_entropy_loss
 
 
-def randn(div: float, seed: int, *shape: int):
-    fn = jax.pmap(lambda x: jax.random.normal(jax.random.PRNGKey(x + seed), shape) / div)
-    local_devices = jax.local_device_count()
-    seeds = jnp.arange(local_devices * jax.process_index(), local_devices * (1 + jax.process_index()))
-    return fn(seeds)
+def randn_fn():
+    rng = random.Random(0)
+
+    def _fn(*shape: int):
+        seed = rng.randint(0, 2 ** 30)
+        div = (shape[-1] * jax.device_count()) ** 0.25
+        fn = jax.pmap(lambda x: jax.random.normal(jax.random.PRNGKey(x + seed), shape) / div)
+        local_devices = jax.local_device_count()
+        seeds = jnp.arange(local_devices * jax.process_index(), local_devices * (1 + jax.process_index()))
+        return fn(seeds)
+
+    return _fn
 
 
 def mean(x: jnp.ndarray):
@@ -26,39 +34,59 @@ def naive_loss(x, y, z_loss):
     lse = jax.nn.logsumexp(tmp, -1)
     pos = mean(lse)
     neg = mean(jnp.take_along_axis(tmp.reshape(-1, tmp.shape[-1]), y.reshape(-1, 1), -1))
-    return pos - neg + mean(lse ** 2 * z_loss)
+    z_loss = mean(lse ** 2 * z_loss)
+    z_loss = z_loss - lax.stop_gradient(z_loss)
+    return pos - neg + z_loss
 
 
-@pytest.mark.parametrize("z_loss", [1, 0.01, 0])
-@pytest.mark.parametrize("samples", [2 ** 10, 2 ** 16])
-def test_loss(z_loss: float, samples: int, trials: int = 16):
+def initialize(z_loss: float, samples: int):
     ctx = Context()
     ctx.training.z_loss = z_loss
     ctx.dims.sequence = int(samples ** 0.5)
     ctx.dims.batch = int(samples ** 0.5)
 
-    key = jax.random.PRNGKey(0)
     rng = random.Random(0)
-    k0, k1, k2 = jax.random.split(key, 3)
-    tgt = jax.random.randint(k0, (ctx.dims.batch, ctx.dims.sequence), 0, ctx.dims.vocab)
+    tgt = jax.random.randint(jax.random.PRNGKey(0), (ctx.dims.batch, ctx.dims.sequence), 0, ctx.dims.vocab)
 
+    return ctx, rng, tgt, randn_fn()
+
+
+def statistics(name: str, var: jnp.ndarray):
     _fn = jax.pmap(lambda x: (lax.pmax(x.max(), "i"), lax.pmin(x.min(), "i"), lax.pmean(x.mean(), "i"),
                               lax.pmean(jnp.square(x - x.mean()).mean(), "i")), "i")
+    max, min, mean, std = [float(a[0]) for a in _fn(var)]
+    if is_main():
+        print(f"{name}: {max=}, {min=}, {mean=}, {std=}")
 
-    def statistics(name: str, var: jnp.ndarray):
-        max, min, mean, std = [float(a[0]) for a in _fn(var)]
-        if is_main():
-            print(f"{name}: {max=}, {min=}, {mean=}, {std=}")
+
+@pytest.mark.parametrize("z_loss", [1, 0.01, 0])
+@pytest.mark.parametrize("samples", [2 ** 10, 2 ** 16])
+def test_value(z_loss: float, samples: int, trials: int = 16):
+    ctx, rng, tgt, randn = initialize(z_loss, samples)
+
+    for i in range(trials):
+        src = randn(ctx.dims.batch, ctx.dims.sequence, ctx.dims.features)
+        wgt = randn(ctx.dims.features, ctx.dims.vocab)
+
+        grad0 = float(jax.pmap(lambda x: cross_entropy_loss(ctx, x, tgt)[0], ParallelAxes.model)((src, wgt))[0])
+        grad1 = float(jax.pmap(lambda x: naive_loss(x, tgt, z_loss), ParallelAxes.model)((src, wgt))[0])
+        assert np.isclose(grad0, grad1)
+
+
+@pytest.mark.parametrize("z_loss", [1, 0.01, 0])
+@pytest.mark.parametrize("samples", [2 ** 10, 2 ** 16])
+def test_grad(z_loss: float, samples: int, trials: int = 1):
+    ctx, rng, tgt, randn = initialize(z_loss, samples)
 
     for i in range(trials):
         if is_main():
             print(f"\n\nAttempt: {i}\n")
-        div = (ctx.dims.features * ctx.dims.heads) ** 0.25
-        src = randn(div, rng.randint(0, 2 ** 30), ctx.dims.batch, ctx.dims.sequence, ctx.dims.features)
-        wgt = randn(div, rng.randint(0, 2 ** 30), ctx.dims.features, ctx.dims.vocab)
+        src = randn(ctx.dims.batch, ctx.dims.sequence, ctx.dims.features)
+        wgt = randn(ctx.dims.features, ctx.dims.vocab)
 
         grad0 = jax.pmap(jax.grad(lambda x: cross_entropy_loss(ctx, x, tgt)[0]), ParallelAxes.model)((src, wgt))
         grad1 = jax.pmap(jax.grad(lambda x: naive_loss(x, tgt, z_loss)), ParallelAxes.model)((src, wgt))
+
         for g0, g1 in zip(grad0, grad1):
             statistics("Grad0", g0)
             statistics("Grad1", g1)
