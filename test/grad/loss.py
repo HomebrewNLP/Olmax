1d5ba46ab875bd9f79009266b3d5e2121b68456e
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-08-23 14:44:18 +0200

    fix(test/loss): resolve OOM + improve style

--- test/grad/loss.py
+++ test/grad/loss.py
@@ -7,24 +7,34 @@ from src.context import Context
 from src.model.loss import cross_entropy_loss
 
 
+def random(*shape: int):
+    fn = jax.pmap(lambda x: jax.random.normal(jax.random.PRNGKey(x), shape))
+    local_devices = jax.local_device_count()
+    seeds = jnp.arange(local_devices * jax.process_index(), local_devices * (1 + jax.process_index()))
+    return fn(seeds)
+
+
+def naive_loss(x, y):
+    tmp = lax.psum(matmul(x[0], x[1]), ParallelAxes.model)
+    pos = jax.nn.logsumexp(tmp, -1).mean()
+    neg = jnp.take_along_axis(tmp.reshape(-1, tmp.shape[-1]), y.reshape(-1, 1), -1).mean()
+    return pos - neg
+
+
 def main():
     ctx = Context()
     key = jax.random.PRNGKey(0)
     k0, k1, k2 = jax.random.split(key, 3)
     tgt = jax.random.randint(k0, (ctx.dims.batch, ctx.dims.sequence), 0, ctx.dims.vocab)
     tgt = jax.device_put_replicated(tgt, jax.local_devices())
-    src = jax.random.normal(k1, (ctx.dims.heads, ctx.dims.batch, ctx.dims.sequence, ctx.dims.features))
-    wgt = jax.random.normal(k2, (ctx.dims.heads, ctx.dims.vocab, ctx.dims.features)) / ctx.dims.features
-    grad0 = jax.pmap(lambda x, y: jax.grad(lambda x, y: cross_entropy_loss(ctx, x, y), has_aux=True)(x, y)[0],
-                     ParallelAxes.model, in_axes=((0, 0), 0))((src, wgt), tgt)
+    src = random(ctx.dims.batch, ctx.dims.sequence, ctx.dims.features)
+    wgt = random(ctx.dims.vocab, ctx.dims.features) / ctx.dims.features
 
-    def _loss(x, y):
-        tmp = lax.psum(matmul(x[0], x[1]), ParallelAxes.model)
-        pos = jax.nn.logsumexp(tmp, -1).mean()
-        neg = jnp.take_along_axis(tmp.reshape(-1, ctx.dims.vocab), y.reshape(-1, 1), -1).mean()
-        return pos - neg
+    inp = (src, wgt), tgt
 
-    grad1 = jax.pmap(jax.grad(_loss), ParallelAxes.model, in_axes=((0, 0), 0))((src, wgt), tgt)
+    grad0 = jax.pmap(jax.grad(lambda x, y: cross_entropy_loss(ctx, x, y)[0]), ParallelAxes.model)(inp)
+    grad1 = jax.pmap(jax.grad(naive_loss), ParallelAxes.model)(inp)
+
     for gr0, gr1 in zip(grad0, grad1):  # iterate over src and weight
         print("NEW TENSOR")
         if not jnp.allclose(gr0, gr1):
