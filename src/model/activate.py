3b85d5a95e15aae8898f2ce9fd9be106047444cd
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-08-24 16:41:59 +0200

    tesT(activate): leakyrelu

--- src/model/activate.py
+++ src/model/activate.py
@@ -1,21 +1,15 @@
 import jax
-from jax import lax, numpy as jnp
+from jax import numpy as jnp
 
 from src.context import Context
 
 
-def softplus(inp: jnp.ndarray):
-    return lax.max(inp, jnp.zeros((), dtype=inp.dtype)) + lax.log1p(lax.exp(-lax.abs(inp)))
-
-
 def activate_forward(inp: jnp.ndarray) -> jnp.ndarray:
-    return inp * lax.tanh(softplus(inp))
+    return inp * activate_grad(inp)
 
 
 def activate_grad(inp: jnp.ndarray) -> jnp.ndarray:
-    gate = lax.tanh(softplus(inp))
-    sigmoid = jax.nn.sigmoid(inp)
-    return (1 - sigmoid) * gate + sigmoid * (inp + gate - gate ** 2 * inp)
+    return jnp.where(inp < 0, 0.01, 1)
 
 
 def activate(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
