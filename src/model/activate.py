6f203a1740a1b5942646e4333f93f250ea975f5f
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-08-24 12:19:16 +0200

    feat(model): use Mish

--- src/model/activate.py
+++ src/model/activate.py
@@ -1,15 +1,19 @@
 import jax
-from jax import numpy as jnp
+from jax import lax, numpy as jnp
 
 from src.context import Context
 
 
+def softplus(inp: jnp.ndarray):
+    return lax.max(inp, 0) + lax.log1p(lax.exp(-lax.abs(inp)))
+
+
 def activate_forward(inp: jnp.ndarray) -> jnp.ndarray:
-    return jnp.where(inp > 0, inp, inp * 0.01)
+    return inp * lax.tanh(softplus(inp))
 
 
 def activate_grad(inp: jnp.ndarray) -> jnp.ndarray:
-    return jnp.where(inp > 0, 1, 0.01)
+    return 1 - lax.square(lax.tanh(softplus(inp))) * inp * jax.nn.sigmoid(inp)
 
 
 def activate(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
