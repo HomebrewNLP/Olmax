0f1ebe9866297d84eb066bedf70c76fbaed93642
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-08-27 10:23:27 +0200

    feat(optimizer): check if parameter comes from core loop

--- src/optimizer.py
+++ src/optimizer.py
@@ -97,13 +97,13 @@ def grafted_shampoo(ctx: Context, weight_update: jnp.ndarray, grad: jnp.ndarray,
     return graft(weight_update, shampoo_update)
 
 
-def is_stacked(ctx: Context, val: jnp.ndarray):
-    return val.shape[0] == ctx.dims.depth
+def is_stacked(ctx: Context, param_name: str, val: jnp.ndarray):
+    return val.shape[0] == ctx.dims.depth and "/step:" in param_name
 
 
-def clip_norm(ctx: Context, val: jnp.ndarray, min_norm: float) -> jnp.ndarray:
+def clip_norm(ctx: Context, param_name: str, val: jnp.ndarray, min_norm: float) -> jnp.ndarray:
     val = lax.square(val)
-    if is_stacked(ctx, val):
+    if is_stacked(ctx, param_name, val):
         val = val.sum(tuple(range(1, val.ndim))).reshape((-1,) + (1,) * (val.ndim - 1))
     else:
         val = val.sum()
@@ -111,8 +111,8 @@ def clip_norm(ctx: Context, val: jnp.ndarray, min_norm: float) -> jnp.ndarray:
 
 
 def adaptive_gradient_clipping(ctx: Context, param_name: str, grad: jnp.ndarray) -> jnp.ndarray:
-    grd_norm = clip_norm(ctx, grad, ctx.optimizer.epsilon)
-    wgt_norm = clip_norm(ctx, ctx.parameters[param_name], 1e-3)
+    grd_norm = clip_norm(ctx, param_name, grad, ctx.optimizer.epsilon)
+    wgt_norm = clip_norm(ctx, param_name, ctx.parameters[param_name], 1e-3)
     grad_scale = jnp.minimum(wgt_norm / grd_norm * ctx.optimizer.gradient_clip, 1)
     return grad * grad_scale
 
@@ -150,7 +150,7 @@ def update(ctx: Context, grads: typing.Dict[str, jnp.ndarray], step: jnp.ndarray
                 weight_update = sm3(ctx, grad)
             if not small_parameter(param_name, grad):
                 if ctx.optimizer.use_shampoo:
-                    if is_stacked(ctx, grad):
+                    if is_stacked(ctx, param_name, grad):
                         weight_update = jnp.stack([grafted_shampoo(ctx, weight_update, grad[i], step)
                                                    for i in range(grad.shape[0])], 0)
                     else:
