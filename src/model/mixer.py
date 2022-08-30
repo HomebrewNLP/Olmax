91a3edeb711e0a028979057af1033197e5d4a794
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2022-08-31 01:56:24 +0200

    fix(mixer): maximal offset

--- src/model/mixer.py
+++ src/model/mixer.py
@@ -13,21 +13,19 @@ from src.model.norm import prenorm
 def mix(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
     original_shape = inp.shape
     items = math.ceil(math.log(ctx.dims.sequence, ctx.dims.spatial_mixing_kernel))
-    max_dim = math.floor(math.log(ctx.dims.sequence, ctx.dims.spatial_mixing_kernel))
-    inp = inp.reshape(ctx.dims.batch, -1, *[ctx.dims.spatial_mixing_kernel] * max_dim, ctx.dims.features)
-    inp = inp.transpose(0, items + 1, *range(1, 1 + items))
-    shape = list(inp.shape)
-    shape[2:] = shape[2:][::-1]
+    samples = 2 ** (int(math.log2(ctx.dims.sequence)) // items)
+    inp = inp.reshape(ctx.dims.batch, -1, ctx.dims.spatial_mixing_kernel, ctx.dims.features)
+    inp = inp.transpose(0, 3, 1, 2)
+    shape = [ctx.dims.batch, ctx.dims.features, -1, samples ** (items - 1)]
     weight_shape = [ctx.dims.spatial_mixing_kernel] * 2
     for i in range(items):
         mask = jnp.triu(jnp.ones(weight_shape, dtype=ctx.model.computation_dtype)) if ctx.model.autoregressive else 1
         wgt = get_param(ctx, f"mix_{i}", weight_shape) * mask
         if ctx.is_initializing:
             continue
-        if i != items - 1:
-            inp = inp.reshape(*shape)
         if i != 0:
-            inp = inp.transpose(0, 1, 1 + items, *range(2, 1 + items))
+            inp = inp.reshape(*shape)
+            inp = inp.transpose(0, 1, 3, 2)
             inp = activate(ctx, inp)
         inp = matmul(inp, wgt)
     return inp.transpose(0, 1 + items, *range(2, 1 + items), 1).reshape(original_shape)
