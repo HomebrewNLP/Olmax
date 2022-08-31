58f2116080a2441994ff0426f44789d36bc4f77c
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-08-31 12:51:46 +0200

    fix(mixer): mask internally

--- src/model/mixer.py
+++ src/model/mixer.py
@@ -1,5 +1,3 @@
-import math
-
 from jax import numpy as jnp
 
 from src.backend import get_param, matmul, with_context
@@ -14,7 +12,7 @@ def mix(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
     original_shape = inp.shape
     weight_shape = [ctx.dims.spatial_mixing_kernel] * 2
     mask = jnp.triu(jnp.ones(weight_shape, dtype=ctx.model.computation_dtype)) if ctx.model.autoregressive else 1
-    weights = [get_param(ctx, f"mix_{i}", weight_shape, scale=mask) for i in range(ctx.model.mixer_iterations)]
+    weights = [get_param(ctx, f"mix_{i}", weight_shape) for i in range(ctx.model.mixer_iterations)]
     if ctx.is_initializing:
         return inp
 
@@ -24,7 +22,7 @@ def mix(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
     transposed_shape = list(shape)
     transposed_shape[3], transposed_shape[2] = transposed_shape[2], transposed_shape[3]
     for i, wgt in enumerate(weights):
-        wgt = wgt
+        wgt = wgt * mask
         if i != 0:
             inp = inp.reshape(*transposed_shape)
             inp = activate(ctx, inp)
