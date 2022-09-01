e3894fce349563809e673ef1a497a476f9bd8d34
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2022-09-01 15:12:40 +0200

    style(mixer): use only einsum and reshape (-> faster?)

--- src/model/mixer.py
+++ src/model/mixer.py
@@ -1,3 +1,5 @@
+import math
+
 from jax import numpy as jnp
 
 from src.backend import get_param, matmul, with_context
@@ -11,24 +13,20 @@ from src.model.norm import prenorm
 def mix(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
     original_shape = inp.shape
     weight_shape = [ctx.dims.spatial_mixing_kernel] * 2
+    max_dims = math.ceil(math.log(ctx.dims.sequence, ctx.dims.spatial_mixing_kernel))
     mask = jnp.triu(jnp.ones(weight_shape, dtype=ctx.model.computation_dtype)) if ctx.model.autoregressive else 1
     weights = [get_param(ctx, f"mix_{i}", weight_shape, std=1, scale=ctx.dims.spatial_mixing_kernel ** -0.5)
-               for i in range(ctx.model.mixer_iterations)]
+               for i in range(max_dims)]
 
     if ctx.is_initializing:
         return inp
 
-    inp = inp.reshape(ctx.dims.batch, -1, ctx.dims.spatial_mixing_kernel, ctx.dims.features)
-    inp = inp.transpose(0, 3, 1, 2)
-    shape = inp.shape
-    transposed_shape = list(shape)
-    transposed_shape[3], transposed_shape[2] = transposed_shape[2], transposed_shape[3]
-    inp = jnp.einsum("bfrs,sz,sz->bfrz", inp, weights[0], mask)
-    for wgt in weights[1:]:
-        inp = activate(ctx, inp)
-        inp = inp.reshape(*transposed_shape)
-        inp = jnp.einsum("bfsr,sz,sz->bfrz", inp, wgt, mask)
-    for _ in range(len(weights) - 1):
-        inp = inp.transpose(0, 1, 3, 2)
-        inp = inp.reshape(*shape)
-    return inp.transpose(0, 2, 3, 1).reshape(original_shape)
+    inp = inp.reshape(ctx.dims.batch, -1, *[ctx.dims.spatial_mixing_kernel] * max_dims, ctx.dims.features)
+    original_dims = ''.join(chr(ord('a') + i) for i in range(len(inp.ndim)))
+    for i, wgt in enumerate(weights):
+        new_dims = original_dims[:i + 2] + "z" + original_dims[i + 3:]
+        reduced_dim = original_dims[i + 2]
+        if i > 0:
+            inp = activate(inp)
+        inp = jnp.einsum(f"{original_dims},{reduced_dim}z,{reduced_dim}z->{new_dims}", inp, wgt, mask)
+    return inp.reshape(original_shape)
