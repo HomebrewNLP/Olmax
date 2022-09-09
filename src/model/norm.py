53b507381b135ec04a107da45d2d3d7cce240f48
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-09-09 21:28:45 +0200

    perf(norm): use faster scalenorm

--- src/model/norm.py
+++ src/model/norm.py
@@ -26,15 +26,13 @@ def norm_forward(ctx: Context, src: jnp.ndarray, wgt: typing.Optional[jnp.ndarra
     src_fp64 = promote_to(src, run_type)
     if psum:
         src_fp64 = lax.psum(src_fp64, axis_name=ParallelAxes.model)
-    mean = src_fp64.mean(-1, keepdims=True)
-    std = stable_rsqrt(jnp.square(src_fp64).sum(-1, keepdims=True) - src.shape[-1] * jnp.square(mean),
-                       ctx.model.norm_eps)
-    norm_out = (src_fp64 - mean) * std
+    std = stable_rsqrt(jnp.square(src_fp64).sum(-1, keepdims=True), ctx.model.norm_eps)
+    norm_out = src_fp64 * std
     out = norm_out * wgt.reshape((1,) * (src.ndim - 1) + (-1,))
     if act:
         out = activate_forward(out)
     out = out.astype(original_dtype)
-    return out, norm_out, std
+    return out, norm_out
 
 
 @with_context()
@@ -55,7 +53,7 @@ def scale_norm_act(ctx: Context, inp: jnp.ndarray, feature_dim: int, weight: typ
     @jax.custom_gradient
     def _fn(src: jnp.ndarray, wgt: jnp.ndarray):
         original_dtype = src.dtype
-        out, norm_out, std = norm_forward(ctx, src, wgt, psum, act)
+        out, norm_out = norm_forward(ctx, src, wgt, psum, act)
 
         def _grad(dy: jnp.ndarray) -> typing.Tuple[jnp.ndarray, jnp.ndarray]:
             norm_out_fp64 = promote_to(norm_out, run_type)
@@ -64,9 +62,9 @@ def scale_norm_act(ctx: Context, inp: jnp.ndarray, feature_dim: int, weight: typ
             if act:
                 dy = dy * activate_grad(norm_out_fp64 * reshaped_weight)
             d_wgt = (dy * norm_out_fp64).sum(list(range(src.ndim - 1))).reshape((-1,))
-            dy = dy * std * reshaped_weight
-            dy -= (dy * norm_out_fp64).mean(-1, keepdims=True) * norm_out_fp64 * src.shape[-1]  # "undo" l2norm
-            dy -= dy.mean(-1, keepdims=True)
+            square = jnp.square(norm_out)
+            square = square.sum(-1, keepdims=True) - square
+            dy = dy * lax.integer_pow(std, 3) * reshaped_weight * square
             if psum:
                 dy = lax.psum(dy, axis_name=ParallelAxes.model)
             return dy.astype(original_dtype), d_wgt
