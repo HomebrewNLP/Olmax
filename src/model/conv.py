85cd6db81f81375c9155e2f0d81a4628fa9948d5
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2022-09-12 07:11:05 +0200

    fix(model): reduce conv weight scale

--- src/model/conv.py
+++ src/model/conv.py
@@ -13,7 +13,7 @@ def conv(ctx: Context, inp: jnp.ndarray, conv_kernel: int, scale: float, in_feat
     fan_in = fan_in / fan_in.sum()
     fan_in = fan_in.reshape(1, 1, -1)
     weight = get_param(ctx, "weight", [out_features, in_features], column_axes=2, scale=scale,
-                       lr_scale=fan_in, stacked_dims=[conv_kernel], transpose=[1, 0, 2], in_out_axis=(0, 1))
+                       scale=fan_in, stacked_dims=[conv_kernel], transpose=[1, 0, 2], in_out_axis=(0, 1))
     if ctx.is_initializing:
         return jnp.zeros(inp.shape[:-1] + (out_features,))
     return lax_conv(inp, weight, [(conv_kernel - 1, 0)], 1)
