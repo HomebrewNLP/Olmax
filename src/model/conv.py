08fed6eb55af25245940bfa27e1c4ae45e3858fe
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-09-16 21:08:55 +0200

    style(model): remove scales

--- src/model/conv.py
+++ src/model/conv.py
@@ -7,13 +7,12 @@ from src.model.norm import prenorm, scale_norm_act
 
 
 @with_context()
-def conv(ctx: Context, inp: jnp.ndarray, conv_kernel: int, scale: float, in_features: int, out_features: int):
+def conv(ctx: Context, inp: jnp.ndarray, conv_kernel: int, in_features: int, out_features: int):
     fan_in = jnp.arange(conv_kernel, 0, -1, dtype=ctx.model.storage_dtype)
     fan_in = (1 - 1 / (conv_kernel * ctx.model.conv_scale + ctx.model.conv_shift)) ** fan_in
     fan_in = fan_in / fan_in.sum()
     fan_in = fan_in.reshape(1, 1, -1)
-    weight = get_param(ctx, "weight", [out_features, conv_kernel, in_features], column_axes=2, scale=scale,
-                       lr_scale=fan_in)
+    weight = get_param(ctx, "weight", [out_features, conv_kernel, in_features], column_axes=2, lr_scale=fan_in)
     if ctx.is_initializing:
         return jnp.zeros(inp.shape[:-1] + (out_features,))
     return lax_conv(inp, weight, [(conv_kernel - 1, 0)], 1)
@@ -22,21 +21,17 @@ def conv(ctx: Context, inp: jnp.ndarray, conv_kernel: int, scale: float, in_feat
 @prenorm
 @with_context()
 def bottleneck_block(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
-    inp = conv(ctx, inp, ctx.dims.outer_bottleneck_kernel, ctx.optimizer.scale.bottleneck,
-               ctx.dims.features, ctx.dims.inner_bottleneck_features)
+    inp = conv(ctx, inp, ctx.dims.outer_bottleneck_kernel, ctx.dims.features, ctx.dims.inner_bottleneck_features)
     inp = scale_norm_act(ctx, inp, ctx.dims.inner_bottleneck_features, psum=True)
-    inp = conv(ctx, inp, ctx.dims.inner_bottleneck_kernel, ctx.optimizer.scale.bottleneck,
-               ctx.dims.inner_bottleneck_features, ctx.dims.inner_bottleneck_features)
+    inp = conv(ctx, inp, ctx.dims.inner_bottleneck_kernel, ctx.dims.inner_bottleneck_features,
+               ctx.dims.inner_bottleneck_features)
     inp = scale_norm_act(ctx, inp, ctx.dims.inner_bottleneck_features)
-    return conv(ctx, inp, ctx.dims.outer_bottleneck_kernel, ctx.optimizer.scale.bottleneck,
-                ctx.dims.inner_bottleneck_features, ctx.dims.features)
+    return conv(ctx, inp, ctx.dims.outer_bottleneck_kernel, ctx.dims.inner_bottleneck_features, ctx.dims.features)
 
 
 @prenorm
 @with_context()
 def dense_block(ctx: Context, inp: jnp.ndarray) -> jnp.ndarray:
-    inp = conv(ctx, inp, ctx.dims.pointwise_kernel, ctx.optimizer.scale.pointwise, ctx.dims.features,
-               ctx.dims.pointwise_features)
+    inp = conv(ctx, inp, ctx.dims.pointwise_kernel, ctx.dims.features, ctx.dims.pointwise_features)
     inp = activate(inp)
-    return conv(ctx, inp, ctx.dims.pointwise_kernel, ctx.optimizer.scale.pointwise, ctx.dims.pointwise_features,
-                ctx.dims.features)
+    return conv(ctx, inp, ctx.dims.pointwise_kernel, ctx.dims.pointwise_features, ctx.dims.features)
