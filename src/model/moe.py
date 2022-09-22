2f561c5183c647faa070f78fb1963cdc882463a1
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-09-22 23:40:15 +0200

    fix(moe): keep unsqueezed

--- src/model/moe.py
+++ src/model/moe.py
@@ -94,7 +94,7 @@ def binary_tree_gating(ctx: Context, inp: jnp.ndarray) -> typing.Tuple[jnp.ndarr
     gates *= (indices > ctx.dims.sequence // 2) * 2 - 1
     gates = (1 + gates - lax.stop_gradient(gates)).prod(-1, keepdims=True)
     for i in range(indices.shape[-1] - 1):
-        indices = jnp.take_along_axis(indices[:, :, 1:], indices[:, :, 0], 1)
+        indices = jnp.take_along_axis(indices[:, :, 1:], indices[:, :, :1], 1)
     return jnp.take_along_axis(inp, indices, 1) * gates, indices
 
 
