fix(inference): reset parameter variance

inference.py
@@ -77,6 +77,7 @@ def body_fn(while_ctx_dict: typing.Dict[str, typing.Any]) -> typing.Dict[str, ty
     out_token = jnp.argmax(out_token, -1)
     wctx.data = jnp.where(one_hot(wctx.current_step, wctx.ctx.dims.sequence).reshape(1, -1), out_token, wctx.data)
     wctx.current_step += 1
+    wctx.ctx.parameter_variance = {}
     return wctx.serialize()
 
 
@@ -109,7 +110,6 @@ class Inference:
     def __init__(self, ctx: Context):
         ctx.is_initializing = True
         dummy_data = np.zeros((1, ctx.dims.sequence), dtype=np.int32)
-        get_parameters(ctx, dummy_data)
         read_checkpoint(ctx)
         self.parameters = ctx.parameters
 