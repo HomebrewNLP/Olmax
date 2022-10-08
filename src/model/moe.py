b1be6ac65025db05a4af1de510b37db83adb30de
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-10-08 13:29:26 +0200

    perf(moe): use  axis index groups instead of roll (+2% speed)

--- src/model/moe.py
+++ src/model/moe.py
@@ -1,32 +1,30 @@
 import jax
 from jax import lax, numpy as jnp
 
-from src.backend import with_context
+from src.backend import pattern_match, with_context
 from src.constants import ParallelAxes
 from src.context import Context
 from src.model.conv import conv
 from src.model.norm import prenorm, scale_norm_act
 
 
-def all_to_all(ctx: Context, x: jnp.ndarray, split_axis: int, concat_axis: int) -> jnp.ndarray:
+def all_to_all(ctx: Context, x: jnp.ndarray, split_axis: int, concat_axis: int, device_shift: int) -> jnp.ndarray:
     if ctx.is_initializing:
         return x
+
+    devices = list(range(jax.device_count()))
+    devices = devices[device_shift:] + devices[:device_shift]
 
     @jax.custom_gradient
     def _fn(inp: jnp.ndarray):
         def _grad(dy: jnp.ndarray) -> jnp.ndarray:
-            return lax.all_to_all(dy, ParallelAxes.model, concat_axis, split_axis, tiled=True)
+            return lax.all_to_all(dy, ParallelAxes.model, concat_axis, split_axis, tiled=True,
+                                  axis_index_groups=devices)
 
-        return lax.all_to_all(inp, ParallelAxes.model, split_axis, concat_axis, tiled=True), _grad
+        return lax.all_to_all(inp, ParallelAxes.model, split_axis, concat_axis, tiled=True,
+                              axis_index_groups=devices), _grad
 
     return _fn(x)
-
-
-def roll(x: jnp.ndarray, amount: jnp.ndarray, axis: int):
-    """
-    https://github.com/google/jax/blob/2693afa263ed651404098fd98ea15b2a8c605a9e/jax/_src/numpy/lax_numpy.py#L3349-L3368
-    """
-    return lax.dynamic_slice_in_dim(lax.concatenate((x, x), axis), x.shape[axis] - amount, x.shape[axis], axis=axis)
 
 
 @prenorm
@@ -42,8 +40,7 @@ def dense_moe(ctx: Context, inp: jnp.ndarray, depth: jnp.ndarray) -> jnp.ndarray
     # In essence, 1) Collect features from all devices + 2) Drop unused sequence elements
     if not ctx.is_initializing:
         inp = inp.reshape(ctx.dims.batch, sequence_slice, devices, ctx.dims.inner_bottleneck_features)
-        inp = roll(inp, depth % devices, 2)
-        inp = all_to_all(ctx, inp, 2, 3)
+        inp = pattern_match(lambda d: lambda x: all_to_all(ctx, x, 2, 3, d), jax.device_count(), depth, inp)
         inp = inp.reshape(ctx.dims.batch, sequence_slice, big_params)
 
     # Devices^2 more parameters than normal bottleneck block but only Devices-times more flops due to sparsity above
@@ -54,8 +51,7 @@ def dense_moe(ctx: Context, inp: jnp.ndarray, depth: jnp.ndarray) -> jnp.ndarray
     # [Batch, SequenceSlice, Features * Devices]  ->  [Batch, Sequence, Features]  (PixelShuffle across devices)
     if not ctx.is_initializing:
         inp = inp.reshape(ctx.dims.batch, sequence_slice, 1, big_params)
-        inp = all_to_all(ctx, inp, 3, 2)
-        inp = roll(inp, (-depth) % devices, 2)
+        inp = pattern_match(lambda d: lambda x: all_to_all(ctx, x, 3, 2, d), jax.device_count(), depth, inp)
         inp = inp.reshape(ctx.dims.batch, ctx.dims.sequence, ctx.dims.inner_bottleneck_features)
 
     return conv(ctx, inp, ctx.dims.outer_bottleneck_kernel, ctx.dims.inner_bottleneck_features, ctx.dims.features)
