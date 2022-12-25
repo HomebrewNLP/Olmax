b194d268e6a4da5f8ecc47a82bee207195793bbf
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-12-25 12:00:46 +0100

    style(norm): early exit

--- src/model/norm.py
+++ src/model/norm.py
@@ -32,7 +32,6 @@ def all_gather(inp: jax.Array, dim: int) -> jax.Array:
 
 def norm_forward(ctx: Context, src: jax.Array, wgt: Optional[jax.Array] = None, psum: bool = False,
                  act: bool = True, dim: int = 2):
-    original_dtype = src.dtype
     run_type = jnp.promote_types(ctx.model.computation_dtype, jnp.float32)
     src_fp64 = promote_to(src, run_type)
     own_sum = lax.square(src_fp64).sum(dim, keepdims=True)
@@ -42,7 +41,7 @@ def norm_forward(ctx: Context, src: jax.Array, wgt: Optional[jax.Array] = None, 
     out = src_fp64 * std * wgt
     if act:
         out = activate_forward(out)
-    out = out.astype(original_dtype)
+    out = out.astype(src.dtype)
     if psum:
         out = all_gather(out, dim)
     return out, std
@@ -65,39 +64,37 @@ def scale_norm_act(ctx: Context, inp: jax.Array, feature_dim: int,
 
     @jax.custom_gradient
     def _fn(src: jax.Array, wgt: jax.Array, _wgt_dummy: jax.Array):
-        original_dtype = src.dtype
         if isinstance(wgt, jax.Array):
-            reshaped_weight = wgt.reshape((1,) * dim + (-1,) + (1,) * (src.ndim - 1 - dim))
-        else:
-            reshaped_weight = wgt
+            wgt = wgt.reshape((1,) * dim + (-1,) + (1,) * (src.ndim - 1 - dim))
 
-        out, std = norm_forward(ctx, src, reshaped_weight, psum, act, dim)
+        out, std = norm_forward(ctx, src, wgt, psum, act, dim)
 
-        def _grad(dy: jax.Array) -> Tuple[jax.Array, jax.Array, jax.Array]:
+        def _grad(dy: jax.Array) -> Union[Tuple[jax.Array, jax.Array, jax.Array], Tuple[jax.Array, None, None]]:
             inner_src = lax.all_gather(src, ParallelAxes.model, axis=dim) if psum else src
             src_fp64 = promote_to(inner_src, run_type)
             norm_out = src_fp64 * std
             dy = promote_to(dy, run_type)
             if act:
-                dy = dy * activate_grad(norm_out * reshaped_weight)
-            if isinstance(wgt, jax.Array):
-                summed = list(range(src.ndim))
-                del summed[dim]
-                d_wgt = dy * norm_out
-                d_wgt_sq = (lax.square(d_wgt).sum(summed) * ctx.dims.batch).reshape((-1,)).astype(run_type)
-                d_wgt = d_wgt.sum(summed).reshape((-1,)).astype(run_type)
-            else:
-                d_wgt = None
-                d_wgt_sq = None
-            dy = dy * reshaped_weight
+                dy = dy * activate_grad(norm_out * wgt)
+            d_normed = dy * wgt
 
-            d_std = (dy * src_fp64).sum(dim, keepdims=True)  # broadcast forward -> sum backward
+            d_std = (d_normed * src_fp64).sum(dim, keepdims=True)  # broadcast forward -> sum backward
             d_std *= std ** 3  # reciprocal + x^(1/pow) -> 1/std^2 * 1/std^(pow-1) * 1/pow
             d_std *= src_fp64  # x^pow -> pow * x^(pow-1), multiply fused with above
-            dx = dy * std - d_std
+            dx = d_normed * std - d_std
             if psum:
                 dx = lax.psum_scatter(dx, axis_name=ParallelAxes.model, scatter_dimension=dim, tiled=True)
-            return dx.astype(original_dtype), d_wgt, d_wgt_sq
+            dx = dx.astype(src.dtype)
+
+            if not isinstance(wgt, jax.Array):
+                return dx, None, None
+
+            summed = list(range(src.ndim))
+            del summed[dim]
+            d_wgt = dy * norm_out
+            d_wgt_sq = (lax.square(d_wgt).sum(summed) * ctx.dims.batch).reshape((-1,)).astype(run_type)
+            d_wgt = d_wgt.sum(summed).reshape((-1,)).astype(run_type)
+            return dx, d_wgt, d_wgt_sq
 
         return out, _grad
 
