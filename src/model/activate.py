4b8eb08b93c985830b211001d206ad51ea912d79
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-02-08 11:10:58 +0100

    fix(activate): casting issue

--- src/model/activate.py
+++ src/model/activate.py
@@ -1,12 +1,12 @@
 import jax
-from jax import lax
+from jax import lax, numpy as jnp
 
 
 # [Rethinking Channel Dimensions for Efficient Model Design](https://arxiv.org/abs/2007.00992) showed that
 # "complicated" activation functions such as SiLU, GeLU and Mish help increase the rank of the embedding matrix.
 # To ensure low-redundancy features, we use a function from that family.
 def _tanh_sp(inp: jax.Array) -> jax.Array:  # jax.nn.softplus without nan check
-    return lax.tanh(lax.max(inp, 0.) + lax.log1p(lax.exp(-lax.abs(inp))))
+    return lax.tanh(jnp.maximum(inp, 0.) + lax.log1p(lax.exp(-lax.abs(inp))))
 
 
 def activate_forward(inp: jax.Array) -> jax.Array:
