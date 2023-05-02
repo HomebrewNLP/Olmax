1e37ec14dd21fa0daf815637efb701c138fa2ff9
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-02 21:02:41 +0200

    fix(setup):

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --no-cache-dir --force-reinstall --upgrade "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-sudo python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
-python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
 python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils
-python3 -m pip install --upgrade tensorflow
