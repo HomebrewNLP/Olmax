d90af4e67260b8bda77f7fd242bffe20d167967e
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-02 19:21:08 +0200

    test(setup): upgrade dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --no-cache-dir --force-reinstall --upgrade "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
+python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
 python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils
-python3 -m pip install --upgrade --force-reinstall tensorflow==2.8.0 protobuf==3.20.1
+python3 -m pip install --upgrade --force-reinstall tensorflow protobuf
