3c23261b9cbad46774978568e02e9d97547ba3b7
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-05 14:39:11 +0200

    fix(optimizer): bitwise not logical xor

--- src/optimizer.py
+++ src/optimizer.py
@@ -91,7 +91,7 @@ def update(ctx: Context, grads: Dict[str, jax.Array], step: jax.Array):
             continue
 
         name_hash = int.from_bytes(hashlib.blake2b(param_name.encode()).digest()[:4], "little")
-        prng_seed = jnp.logical_xor(step.astype(jnp.int32), name_hash)
+        prng_seed = jnp.bitwise_xor(step.astype(jnp.int32), name_hash)
 
         ctx = outer_ctx.add_to_prefix(param_name, count=False)
         ctx.name_cache = {}
