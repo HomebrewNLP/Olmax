d0f85ced744f855e056313cd2b6bafddb4da252c
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-05 14:43:37 +0200

    fix(optimizer): cant generate fp64 values

--- src/optimizer.py
+++ src/optimizer.py
@@ -99,7 +99,7 @@ def update(ctx: Context, grads: Dict[str, jax.Array], step: jax.Array):
         parameter_lr = lr * ctx.parameter_variance.get(param_name, 1)
 
         grad = grad.astype(jnp.float64)
-        noise = jax.random.normal(jax.random.PRNGKey(prng_seed), grad.shape, grad.dtype)
+        noise = jax.random.normal(jax.random.PRNGKey(prng_seed), grad.shape, ctx.model.storage_dtype).astype(grad.dtype)
         noise *= ctx.optimizer.gradient_noise_factor * grad.std()
         grad += noise
         grad = adaptive_gradient_clipping(ctx, param_name, grad, False)
