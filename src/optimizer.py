e6dbe5fb00d3bfddf165f901e54ae1cca332a02d
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-05 14:37:29 +0200

    fix(optimizer): no int64

--- src/optimizer.py
+++ src/optimizer.py
@@ -89,14 +89,17 @@ def update(ctx: Context, grads: Dict[str, jax.Array], step: jax.Array):
     for param_name, grad in grads.items():
         if "optimizer" in param_name:
             continue
+
         name_hash = int.from_bytes(hashlib.blake2b(param_name.encode()).digest()[:4], "little")
+        prng_seed = jnp.logical_xor(step.astype(jnp.int32), name_hash)
+
         ctx = outer_ctx.add_to_prefix(param_name, count=False)
         ctx.name_cache = {}
         dtype = ctx.parameters[param_name].dtype
         parameter_lr = lr * ctx.parameter_variance.get(param_name, 1)
 
         grad = grad.astype(jnp.float64)
-        noise = jax.random.normal(jax.random.PRNGKey(step.astype(jnp.int64) + name_hash), grad.shape, grad.dtype)
+        noise = jax.random.normal(jax.random.PRNGKey(prng_seed), grad.shape, grad.dtype)
         noise *= ctx.optimizer.gradient_noise_factor * grad.std()
         grad += noise
         grad = adaptive_gradient_clipping(ctx, param_name, grad, False)
