5395c3ca02ba4a4831deb3e69cde38c1bfa99340
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-05 16:03:23 +0200

    test(model): params can be curried if they're not scanned

--- src/model/main.py
+++ src/model/main.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Optional
 
 import jax
 from jax import lax, numpy as jnp
@@ -39,16 +39,18 @@ def loss_fn(ctx: Context, src: REVERSIBLE_CTX, tgt: jax.Array) -> Tuple[REVERSIB
 def block(ctx: Context):
     name_cache = ctx.name_cache
 
-    def _fn(src: Tuple[REVERSIBLE_CTX, jax.Array], inp: Tuple[jax.Array, jax.Array, jax.Array]):
+    def _fn(src: Tuple[Tuple[jax.Array, jax.Array, jax.Array, jax.Array, jax.Array, jax.Array], jax.Array],
+            inp: Tuple[jax.Array, jax.Array, jax.Array]):
         inp, tgt, position = inp
         src, original_loss = src
+        src = (ctx.parameters, *src)
         src = reversible(ctx, read, SparseAccess.read, src, inp, position)
         src = reversible(ctx, write, SparseAccess.write, src, inp, position)
         src, loss = loss_fn(ctx, src, tgt)
         name_cache.update(ctx.name_cache)
         if ctx.is_initializing:
             return src
-        return (src, original_loss + loss), None
+        return (src[1:], original_loss + loss), None
 
     return _fn
 
@@ -61,23 +63,23 @@ def batch_embedding(ctx: Context, name: str, *shape: int) -> Tuple[jax.Array, ja
 
 
 @with_context()
-def body_ctx(ctx: Context, src: jax.Array, tgt: jax.Array) -> Tuple[jax.Array, jax.Array]:
+def body_ctx(ctx: Context, src: jax.Array, tgt: jax.Array) -> Optional[Tuple[jax.Array, jax.Array]]:
     dense0 = batch_embedding(ctx, "dense0", ctx.dims.features)
     dense1 = batch_embedding(ctx, "dense1", ctx.dims.features)
     sparse = batch_embedding(ctx, "sparse", ctx.dims.memory_slots, ctx.dims.memory_features)
-    carry = ((ctx.parameters, *dense0, *dense1, *sparse), jnp.zeros((2,), dtype=jnp.float64))
+    carry = ((*dense0, *dense1, *sparse), jnp.zeros((2,), dtype=jnp.float64))
+    src = src.transpose(1, 0)
+    tgt = tgt.transpose(1, 0)
 
     if ctx.is_initializing:
-        ctx.parameters, *src = block(ctx)(carry, (src[:, 0], tgt[:, 0], jnp.zeros([], dtype=jnp.int32)))
-        return src
+        ctx.parameters, *src = block(ctx)(carry, (src[0], tgt[0], jnp.zeros([], dtype=jnp.int32)))
+        return
 
-    (_, loss), _ = lax.scan(block(ctx), carry,
-                            (src.transpose(1, 0), tgt.transpose(1, 0), jnp.arange(ctx.dims.sequence)),
-                            ctx.dims.sequence)
+    (_, loss), _ = lax.scan(block(ctx), carry, (src, tgt, jnp.arange(ctx.dims.sequence)))
     return loss[0], loss[1]
 
 
-def compute(params: Dict[str, jax.Array], inp: jax.Array) -> Tuple[jax.Array, jax.Array]:
+def compute(params: Dict[str, jax.Array], inp: jax.Array) -> Optional[Tuple[jax.Array, jax.Array]]:
     ctx = Context()
     ctx.parameters = params
     src, tgt = inp
