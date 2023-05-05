050795482055a6d5dd3f80a701b718d993c521c0
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-05 11:10:39 +0200

    fix(norm): iterate correctly

--- src/model/norm.py
+++ src/model/norm.py
@@ -131,7 +131,7 @@ def scale_norm_act_linear(ctx: Context, inp: jax.Array, in_features: int, out_fe
             if len(weights) == 1:
                 dy = [dy]
             out2, norm_out, bw_out, src_fp64, _ = norm_forward(ctx, src, scl, True, dim, False, std)
-            dy, d_wgt = zip(*[jax.vjp(_mm, out2, w)[1](fn[1](dy)) for fn, w, dy in zip(transform_fns, wgt, dy)])
+            dy, d_wgt = zip(*[jax.vjp(_mm, out2, w)[1](fn[1](tmp)) for fn, w, tmp in zip(transform_fns, wgt, dy)])
             dx, d_scl = norm_backward(src, scl, std, sum(dy), act, dim, False, scale.shape, run_type, src_fp64,
                                       norm_out, bw_out)
             return dx, d_scl, d_wgt
