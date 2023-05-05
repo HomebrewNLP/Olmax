a5589e037fd94fcb1b658ad119e760ea065a62ec
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-05 13:45:33 +0200

    style(reversible): move reshape

--- src/model/reversible.py
+++ src/model/reversible.py
@@ -63,8 +63,8 @@ def reversible(ctx: Context, fn: ReversibleFn, sparse_access: SparseAccess, src:
             if sparse_access == SparseAccess.write:
                 x0, vals = x0
                 y_sparse = at_sparse(y_sparse, keys).add(-vals)
-                sparse_items = jnp.take_along_axis(dy_sparse, keys.reshape(*keys.shape, 1), 1)
-                d_params, dx0, _ = grad_fn((dy1, sparse_items.reshape(*vals.shape)))
+                sparse_items = jnp.take_along_axis(dy_sparse, keys.reshape(*keys.shape, 1), 1).reshape(*vals.shape)
+                d_params, dx0, _ = grad_fn((dy1, sparse_items))
             elif sparse_access == SparseAccess.read:
                 d_params, dx0, (dsparse, *_) = grad_fn(dy1)
                 dy_sparse = dy_sparse + dsparse  # TODO: Find a way to get the sparse gradients and scatter-add manually
