85bb18837cb6e24b9be5b13512a6db1db2fd6b0b
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-07 12:43:06 +0200

    fix(norm): correct scalenormact gradient

--- src/model/norm.py
+++ src/model/norm.py
@@ -129,9 +129,10 @@ def scale_norm_act_linear(ctx: Context, inp: jax.Array, in_features: int, out_fe
 
         def _grad(dy: jax.Array) -> Tuple[jax.Array, jax.Array, List[jax.Array]]:
             out2, norm_out, bw_out, src_fp64, _ = norm_forward(ctx, src, scl, True, dim, False, std)
-            dy, d_wgt = zip(*[jax.vjp(_mm, out2, w)[1](fn[1](tmp)) for fn, w, tmp in zip(transform_fns, wgt, dy)])
-            dx, d_scl = norm_backward(src, scl, std, sum(dy), act, dim, False, scale.shape, run_type, src_fp64,
-                                      norm_out, bw_out)
+            dy, d_wgt = zip(*[jax.vjp(_mm, fn[0](out2), w)[1](tmp) for fn, w, tmp in zip(transform_fns, wgt, dy)])
+            dy = sum(fn[1](d) for fn, d in zip(transform_fns, dy))
+            dx, d_scl = norm_backward(src, scl, std, dy, act, dim, False, scale.shape, run_type, src_fp64, norm_out,
+                                      bw_out)
             return dx, d_scl, list(d_wgt)
 
         return [_mm(fn[0](out), w) for fn, w in zip(transform_fns, wgt)], _grad
