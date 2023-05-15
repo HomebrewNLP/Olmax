610c2e80d5e752b83923e1f93fdf7c33bd059700
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-05-15 10:40:57 +0200

    feat(run.sh): add cpu config

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
-export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc.so.4  # faster malloc
-export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=60000000000  # no numpy memory warnings
-export TF_CPP_MIN_LOG_LEVEL=4   # no dataset warnings
+if [ -f /usr/lib/x86_64-linux-gnu/libtcmalloc.so.4 ]; then  # We're on a TPU
+  export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc.so.4  # faster malloc
+  export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=60000000000  # no numpy memory warnings
+  export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+  export XLA_FLAGS="--xla_force_host_platform_device_count=1"  # We don't use TPU-CPU for ML
+else
+  export JAX_PLATFORM_NAME="cpu"  # CUDA is broken 0.4.* + CU12.x, so we enforce CPU
+  export XLA_FLAGS="--xla_force_host_platform_device_count=8"  # Here we _do_ use the host CPU for ML
+fi
 
-export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+export TF_CPP_MIN_LOG_LEVEL=4   # no dataset warnings
 
 export JAX_ENABLE_X64=1  # allow fp64
 export JAX_DEFAULT_DTYPE_BITS=32  # ..but don't enforce it
@@ -11,7 +17,6 @@ export WANDB_WATCH="false"  # workaround to wandb crashing and killing the whole
 export WANDB_START_METHOD="thread"
 
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/compiler/xla/xla.proto
-export XLA_FLAGS="--xla_force_host_platform_device_count=1"  # We don't use TPU-CPU for ML
 # export XLA_FLAGS="--xla_step_marker_location=1 $XLA_FLAGS"  # 0 = entry; 1 = outer while
 
 /usr/bin/env python3 main.py "$@"
